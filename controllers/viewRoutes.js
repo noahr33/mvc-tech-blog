b085a5dbe34220e0bf96bb2e472bfdfbba52d66f
noahr33 <128255532+noahr33@users.noreply.github.com>
2023-09-08 20:57:51 -0500

    blog-posts, bootstrap

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -9,8 +9,20 @@ router.get('/', async (req, res) => {
       include: [User],
       raw: true
     })
-    console.log(blogs)
     res.render('home', { blogs })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/blog-post/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id, {
+      include: [{ model: User }],
+      raw: true,
+    })
+    console.log(blog)
+    res.render('blogpost', blog)
   } catch (err) {
     res.status(500).json(err)
   }
